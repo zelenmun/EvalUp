facca0ddcb5d9ba6cf1feb173e8fdc86c892e594
Zelen <oscar_gomez2018@hotmail.com>
2025-05-23 02:04:42 -0500

    .

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -212,7 +212,7 @@ export default function SignUpForm() {
                                         fill="#EB4335"
                                     />
                                 </svg>
-                                Sign up with Google
+                                Inicia con Google
                             </button>
                             <button
                                 className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
@@ -227,7 +227,7 @@ export default function SignUpForm() {
                                     <path
                                         d="M15.6705 1.875H18.4272L12.4047 8.75833L19.4897 18.125H13.9422L9.59717 12.4442L4.62554 18.125H1.86721L8.30887 10.7625L1.51221 1.875H7.20054L11.128 7.0675L15.6705 1.875ZM14.703 16.475H16.2305L6.37054 3.43833H4.73137L14.703 16.475Z"/>
                                 </svg>
-                                Sign up with X
+                                Inicia con X
                             </button>
                         </div>
                         <div className="relative py-3 sm:py-5">
